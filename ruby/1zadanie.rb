74f9933fede892eff1d759dffd04bf9f6cebf105
Sano <kovalev1998@mail.ru>
2018-07-06 15:42:11 +0300

    zadaniya ruby

--- ruby/1zadanie.rb
+++
@@ -1,7 +0,0 @@
-puts "введите вашу фамилию"
-name = gets.chomp + " "
-puts "введите ваше имя"
-name2 = gets.chomp + ' '
-puts "введите ваше отчество"
-name3 = gets.chomp
-puts 'Здравствуйте, ' + name + name2 + name3
