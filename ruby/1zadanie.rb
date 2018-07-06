7221278371c76111c95a6770611ea1a418f0ee05
Sano <kovalev1998@mail.ru>
2018-07-06 10:57:12 +0300

    zadaniya ruby

---
+++ ruby/1zadanie.rb
@@ -0,0 +1,7 @@
+puts "введите вашу фамилию"
+name = gets.chomp + " "
+puts "введите ваше имя"
+name2 = gets.chomp + ' '
+puts "введите ваше отчество"
+name3 = gets.chomp
+puts 'Здравствуйте, ' + name + name2 + name3
---
+++ ruby/zadanie2.rb
@@ -0,0 +1,4 @@
+puts 'Напишите сколько секунд вы прожили:'
+number = gets.to_i
+number2 = number/31556926
+puts "Ух ты, вы прожили уже " + number2.to_s + " лет!"
