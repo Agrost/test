3dd1e2e2352f5261b6503e3854778abc15e7a2fc
Sano <kovalev1998@mail.ru>
2018-07-06 15:56:51 +0300

    zadaniya ruby

--- ruby/1zzadanie.rb
+++ ruby/1zzadanie.rb
@@ -1,17 +1,17 @@
 def year day
 puts 'Напишите сколько секунд вы прожили:'
 number = gets.to_i
-number2 = number/31556926
-puts number2
-if (number2 == 1 or number2 == 101)
+number3 = (number/31556926)
+number2 = number3 % 10
+if (number2 == 1)
   x="год"
   else
-    if ((number2 < 5 and number2 > 1) or (number2 > 101 and number2 < 105))
+    if (number2 > 1 and number2 < 5)
     x="года"
   else
       x="лет"
     end
 end
-puts "Ух ты, вы прожили уже " + number2.to_s + " " + x.to_s + "!"
+puts "Ух ты, вы прожили уже " + number3.to_s + " " + x.to_s + "!"
 end
 year 1
