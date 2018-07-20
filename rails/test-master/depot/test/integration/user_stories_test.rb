4f9de0727c83e5877c08ca4bf8b5a32487eb93cc
Agrost <kovalev1998@mail.ru>
2018-07-20 11:43:54 +0300

    111

--- rails/test-master/depot/test/integration/user_stories_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class UserStoriesTest < ActionDispatch::IntegrationTest
-end
--- ruby/zadanie3.rb
+++
@@ -1,4 +0,0 @@
-puts 'Напишите сколько секунд вы прожили:'
-number = gets.to_i
-number2 = number/31556926
-puts "Ух ты, вы прожили уже " + number2.to_s + " лет!"
